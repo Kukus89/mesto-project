07f1d61104bddd763e016c6f4b2394e4a77383b3
Kukus89 <kukus89@gmail.com>
2023-01-19 16:42:47 +0300

    Исправление 4

--- scripts/script.js
+++ scripts/script.js
@@ -14,9 +14,9 @@ const popupPreviewImage = document.querySelector('.popup__preview-image');
 const imageSubtitle = document.querySelector('.popup__preview-image-subtitle');
 const popupEditeProfile = document.querySelector('.popup_edite-profile');
 const popupCardAdd = document.querySelector('.popup_card-add');
+const newCardAddPlace = document.querySelector('.elements__list');
 
-// Реализация функциональности проекта «Mesto» на JavaScript
-// 1. Работа модальных окон
+// Открытие попапа профиля
 
 function editeProfileButtonClick() {
   popupProfileName.value = profileName.textContent;
@@ -24,6 +24,16 @@ function editeProfileButtonClick() {
   popupEditeProfile.classList.add('popup_opened');
 }
 buttonEditeProfile.addEventListener('click', editeProfileButtonClick);
+
+// Попап профиля
+
+function handleSubmitEditProfileForm(evt) {
+  evt.preventDefault();
+  profileName.textContent = popupProfileName.value;
+  profileSubtitle.textContent = popupProfileSubtitle.value;
+  popupEditeProfile.classList.remove('popup_opened');
+}
+popupSubmitSaveProfileButton.addEventListener('click', handleSubmitEditProfileForm);
 
 // Закрытие всех попапов "крестиком"
 
@@ -39,17 +49,47 @@ for (i = 0; i < popups.length; i++) {
   buttonsClose[i].addEventListener('click', closePopupButtonClick);
 }
 
-function handleSubmitEditProfileForm(evt) {
-  evt.preventDefault();
-  profileName.textContent = popupProfileName.value;
-  profileSubtitle.textContent = popupProfileSubtitle.value;
-  popupEditeProfile.classList.remove('popup_opened');
+// 3. Попап добавления карточки
+
+function addCardButtonClick() {
+  popupProfileName.value = profileName.textContent;
+  popupProfileSubtitle.value = profileSubtitle.textContent;
+  popupCardAdd.classList.add('popup_opened');
 }
-popupSubmitSaveProfileButton.addEventListener('click', handleSubmitEditProfileForm );
+buttonAddCard.addEventListener('click', addCardButtonClick);
+
+// 4. Добавление карточек
+
+// форма готовой карточки
+function addCard(placeName, srcPic) {
+  const newCardAddPlace = document.querySelector('.elements__list');
+  const newCardTemplate = document.querySelector('.templateElements').content;
+  const element = newCardTemplate.querySelector('.element').cloneNode(true);
+  const elementImage = element.querySelector('.element__image')
+  elementImage.src = srcPic;
+  element.querySelector('.element__title').textContent = placeName;
+  element.querySelector('.element__like').addEventListener('click', like)
+  element.querySelector('.element__delete-button').addEventListener('click', removeCard);
+  elementImage.addEventListener('click', popupImage);
+  return element
+  closePopupButtonClick();
+}
+
+// добавление новой карточки
+function addNewCard() {
+  const imageSrc = document.querySelector('.popup__src-image').value;
+  const popupAreaName = document.querySelector('.popup__area-name').value;
+  newCardAddPlace.prepend(addCard(popupAreaName, imageSrc));
+}
+
+popupSubmitAddCardButton.addEventListener('click', addNewCard);
 
 // 2. Шесть карточек «из коробки»
 
-//Задания не по порядку т.к. вызов функции после её объявления
+initialCards.forEach(element => {
+  newCardAddPlace.prepend(addCard(element.name, element.link));
+});
+
 // 5. Лайк карточки
 
 function like(e) {
@@ -73,47 +113,4 @@ function popupImage(e) {
   popups[2].classList.add('popup_opened');
 }
 
-function addCardDefault(placeName, srcPic) {
-  const newCardTemplate = document.querySelector('.templateElements').content;
-  const newCardAddPlace = document.querySelector('.elements__list');
-  const newCardTemplateElement = newCardTemplate.querySelector('.element').cloneNode(true);
-  newCardTemplateElement.querySelector('.element__image').src = srcPic;
-  newCardTemplateElement.querySelector('.element__title').textContent = placeName;
-  newCardTemplateElement.querySelector('.element__like').addEventListener('click', like);
-  newCardAddPlace.append(newCardTemplateElement);
-  newCardTemplateElement.querySelector('.element__delete-button').addEventListener('click', removeCard);
-  newCardTemplateElement.querySelector('.element__image').addEventListener('click', popupImage);
-}
-for (let i = 0; i < initialCards.length; i++) {
-  addCardDefault(initialCards[i].name, initialCards[i].link)
-}
 
-// 3. Форма добавления карточки
-
-// форма добавлена в разметку т.к. в тренажере не рекомендуется переводить разметку модальных окон в template.
-
-function addCardButtonClick() {
-  popupProfileName.value = profileName.textContent;
-  popupProfileSubtitle.value = profileSubtitle.textContent;
-  popupCardAdd.classList.add('popup_opened');
-}
-buttonAddCard.addEventListener('click', addCardButtonClick);
-
-// 4. Добавление карточки
-
-// При замене события 'клик' на 'Сабмит' происходит обновление страницы, и новая крточка сбрасывается
-
-function addCard(evt) {
-  evt.preventDefault();
-  const newCardAddPlace = document.querySelector('.elements__list');
-  const newCardTemplate = document.querySelector('.templateElements').content;
-  const element = newCardTemplate.querySelector('.element').cloneNode(true);
-  element.querySelector('.element__image').src = document.querySelector('.popup__src-image').value;
-  element.querySelector('.element__title').textContent = document.querySelector('.popup__area-name').value;
-  element.querySelector('.element__like').addEventListener('click', like)
-  element.querySelector('.element__delete-button').addEventListener('click', removeCard);
-  element.querySelector('.element__image').addEventListener('click', popupImage);
-  newCardAddPlace.prepend(element);
-  closePopupButtonClick();
-}
-popupSubmitAddCardButton.addEventListener('submit', addCard);
